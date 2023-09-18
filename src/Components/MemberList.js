ab5825486f972b2241981405d90d380b53a7c57d
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-09-18 17:05:11 +0100

    added borders in member list

--- src/Components/MemberList.js
+++ src/Components/MemberList.js
@@ -6,9 +6,10 @@ const MemberList = ({ tab, privateChats, setTab }) => {
   return (
             <div className="member-list">
                 <ul>
-                    <li onClick={()=>{setTab("CHATROOM")}} className={`member ${tab==="CHATROOM" && "active"}`}>Chatroom</li>
+                    <li onClick={()=>{setTab("CHATROOM")}} className={`member ${tab==="CHATROOM" && "active"} border bg-gray-400 rounded mb-4`}>Chatroom</li>
+                    <li className="rounded mb-4">Private Chats</li>
                     {[...privateChats.keys()].map((name,index)=>(
-                        <li onClick={()=>{setTab(name)}} className={`member ${tab===name && "active"}`} key={index}>{name}</li>
+                        <li onClick={()=>{setTab(name)}} className={`member ${tab===name && "active"} border bg-gray-700 rounded`} key={index}>{name}</li>
                     ))}
                 </ul>
             </div>
