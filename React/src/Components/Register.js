5843efe12e0f2bf7ec4c2f2b8ba929e3c6773b95
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-22 01:03:59 +0100

    button onclicks dont work on mobile

--- React/src/Components/Register.js
+++ React/src/Components/Register.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 const Register = ({userData, handleValue, registerUser}) => {
+  
   return (
     <div className=" flex flex-col items-center h-screen justify-center">
     <h1 className='mb-24 text-6xl text-[#FFFFFF] font-extrabold'>livegab</h1>
     <input
       name="username"
-      className=" w-[30%] h-[5%] border rounded bg-[#282A3A] border-[#7d94b5] text-center mb-4"
+      type='text'
+      className="input-accent w-[30%] h-[5%] border rounded bg-[#282A3A] border-[#7d94b5] text-center mb-4"
       placeholder="Enter your user name"
       value={userData.username}
       onChange={handleValue}
     />
-    <button type="button" onClick={registerUser} className="w-[30%] h-[5%] bg-[#007acc] text-white rounded hover:bg-[#2790d5]">
+    <button
+    id='reg-btn'
+    onClick={registerUser}
+    className="glass w-[30%] h-[5%] cursor:pointer bg-[#007acc] text-white rounded hover:bg-[#2790d5]">
       Join
     </button>
   </div>
