fb9baf8243c1eee087f43e64e26afb0c3f87a76c
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-24 12:16:55 +0100

    styling updated, current user fixed to bottom of sidebar

--- React/src/Components/MemberList.js
+++ React/src/Components/MemberList.js
@@ -7,15 +7,16 @@ const MemberList = ({ tab, privateChats, setTab, userData }) => {
   
 
   return (
-            <div className="member-list cursor-default text-[#8d8f92] text-center">
+            <div className="member-list cursor-default text-[#8d8f92] flex justify-center">
                 <ul>
-                    <li onClick={()=>{setTab("CHATROOM")}} className={`member ${tab==="CHATROOM" && "active"} max-w-[100%] break-words cursor-pointer mb-4`}>Chatroom</li>
-                    <li className=' border-[#ffffff29] mb-2'>Signed in as: {userData.username}</li>
-                    <li className="max-w-[100%] py-4 break-words rounded font-thin md:text-lg text-sm">DIRECT MESSAGES</li>
+                    <li onClick={()=>{setTab("CHATROOM")}} className= 'max-w-[100%] break-words cursor-pointer mt-6  hover:bg-primary hover:border hover:border-rounded hover:rounded-md hover:border-[#36393e] hover:text-[#ffffffb9] font-thin md:text-lg text-sm'>PUBLIC MESSAGES</li>
+                    <li className="max-w-[100%] mt-6 mb-2 break-words rounded font-thin md:text-lg text-sm">DIRECT MESSAGES</li>
                     {[...privateChats.keys()].map((name,index)=>( name !== userData.username ? (
-                    <li onClick={()=>{ {setTab(name)}}} className={`member ${tab===name && "active"} py-1  mr-1 ml-1 cursor-pointer hover:bg-primary hover:border hover:border-rounded hover:rounded-md hover:border-[#36393e] hover:text-[#ffffffb9]`} key={index}> {name === userData.username ? '' : name}  </li> ) : null
+                    <li onClick={()=>{ {setTab(name)}}} className='py-1 break-all mr-1 ml-1 cursor-pointer hover:bg-primary hover:border hover:border-rounded hover:rounded-md hover:border-[#36393e] hover:text-[#ffffffb9]' key={index}> {name === userData.username ? '' : name}  </li> ) : null
                     ))}
                 </ul>
+                <div className='fixed bottom-0 break-all font-thin mb-2 text-center border-[#ffffff29] mt-4'>Signed in as: {userData.username}</div>
+                
 
             </div>
   );
