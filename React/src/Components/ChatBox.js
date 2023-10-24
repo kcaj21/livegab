d1a98439bfb9b19957572a857cc13994458e797d
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2023-10-24 16:55:46 +0100

    added mobile sidebar with toggle

--- React/src/Components/ChatBox.js
+++ React/src/Components/ChatBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import MemberList from './MemberList';
 import ChatContent from './ChatContent';
 import MessageInput from './MessageInput';
@@ -15,58 +15,88 @@ const ChatBox = ({
   onConnected
 }) => {
   const chatContentRef = useRef(null);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const autoScroll = () => {
-    
     if (
-    chatContentRef.current && chatContentRef.current.scrollTop >= (chatContentRef.current.scrollHeight - (window.screen.height*0.92))
+      chatContentRef.current &&
+      chatContentRef.current.scrollTop >=
+        chatContentRef.current.scrollHeight - window.screen.height * 0.92
     ) {
-    chatContentRef.current.scrollTop = chatContentRef.current.scrollHeight}
-  }
+      chatContentRef.current.scrollTop = chatContentRef.current.scrollHeight;
+    }
+  };
 
   const initialScroll = () => {
-    if (
-      chatContentRef.current.scrollTop  <= 10
-      ) 
-      {chatContentRef.current.scrollTop = chatContentRef.current.scrollHeight}
-  }
+    if (chatContentRef.current.scrollTop === 0) {
+      chatContentRef.current.scrollTop = chatContentRef.current.scrollHeight;
+    }
+  };
 
   useEffect(() => {
-    initialScroll()
-  })
+    console.log(document.documentElement.scrollTop)
+    initialScroll();
+  }, );
 
-  
   useEffect(() => {
-
-    autoScroll()
+    autoScroll();
   }, [publicChats, privateChats]);
 
+  const handleSidebarToggle = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <div className="Chatbox h-screen flex flex-col-1">
-      <div className="SideBar w-full max-w-[20%] bg-[#282b30] border-[#ffffff29] text-white shadow-lg">
-        <MemberList tab={tab} privateChats={privateChats} setTab={setTab} userData={userData} />
+    
+    <div className=''>
+      <div className=''>
+      <svg
+  onClick={handleSidebarToggle}
+  className="fixed h-10 w-10 top-0 z-20 sm:hidden block border-2 border-[#424549] rounded-md ml-1 mt-1"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="#8398e6"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    d="M4 6h16M4 12h16m-7 6h7"
+  />
+</svg>
       </div>
-      <div className='h-[90%] w-screen overflow-y-auto' ref={chatContentRef}>
-        <ChatContent 
-          tab={tab}
-          userData={userData}
-          publicChats={publicChats}
-          privateChats={privateChats}
-          handleValue={handleValue}
-          sendPublicMessage={sendPublicMessage}
-          sendPrivateMessage={sendPrivateMessage}
-        />
-      </div>
-      <div className="messageSender mb-4 mx-auto pr-4 h-[8%] w-[79%] fixed bottom-0 right-0">
-        <MessageInput
-          tab={tab}
-          userData={userData}
-          publicChats={publicChats}
-          privateChats={privateChats}
-          handleValue={handleValue}
-          sendPublicMessage={sendPublicMessage}
-          sendPrivateMessage={sendPrivateMessage}
-        />
+      {!sidebarOpen ? null : (
+        <div className='fixed border-r-2 border-[#27292c] left-0 z-10 h-screen bg-[#282b30] w-[60%]'>
+          <MemberList tab={tab} privateChats={privateChats} setTab={setTab} userData={userData} />
+        </div>
+      )}
+
+      <div className='Chatbox h-screen flex flex-col-1'>
+        <div className='SideBar border-r-2 border-[#27292c] w-full max-w-[20%] bg-[#282b30] hidden sm:block border-[#ffffff29] text-white shadow-lg'>
+          <MemberList tab={tab} privateChats={privateChats} setTab={setTab} userData={userData} />
+        </div>
+        <div className='h-[90%] sm:ml-0 ml-8 w-screen overflow-y-auto' ref={chatContentRef}>
+          <ChatContent
+            tab={tab}
+            userData={userData}
+            publicChats={publicChats}
+            privateChats={privateChats}
+            handleValue={handleValue}
+            sendPublicMessage={sendPublicMessage}
+            sendPrivateMessage={sendPrivateMessage}
+          />
+          <div className='messageSender mb-4 pr-4 h-[8%] sm:w-[79%] w-[98%] fixed bottom-0 right-0'>
+            <MessageInput
+              tab={tab}
+              userData={userData}
+              publicChats={publicChats}
+              privateChats={privateChats}
+              handleValue={handleValue}
+              sendPublicMessage={sendPublicMessage}
+              sendPrivateMessage={sendPrivateMessage}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
